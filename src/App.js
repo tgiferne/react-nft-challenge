e8a63c9d07b43defa0dfd01efba5c981c4090103
tgifernee01~ <anferne.thomas@gmail.com>
2022-05-10 23:53:10 -0400

    commit on main

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
 import logo from './logo.svg';
 import './App.css';
+import CollectionCard from './components/CollectionCard';
+import Header from './components/Header';
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+
 
 function App() {
+
+const [punkListData, setPunkListData] = useEffect([])
+
+useEffect(() => {
+  const getMyNfts = async () => {
+    const openseaData = await axios.get('https://testnets-api.opensea.io/assets?assets_contract_address=0x93e19f97744bD1eeA86E70706F1BC510f19e6679&order_direction=acs'
+    )
+    console.log(openseaData.data.assets)
+    setPunkListData(openseaData.data.assets)
+  }
+
+  return getMyNfts()
+}, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='app'>
+      <Header />
+      <CollectionCard 
+      id={0} 
+      name={'Bandana Punk'} 
+      traits={[{'value': 7}]} 
+      image='https://ipfs.thirdweb.com/ipfs/QmZ5fD3UTRh8ALZCpMdypHkhMQSXyi4yyCz3Ea19kPmtXg/0.jpg' />
+
+
     </div>
   );
 }
