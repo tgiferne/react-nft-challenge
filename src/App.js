736dab3c01edc460d242a6e03d8db5b5184dfddf
tgifernee01~ <anferne.thomas@gmail.com>
2022-05-19 02:36:20 -0400

    commit on 'main'

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import logo from './logo.svg';
 import './App.css';
-import CollectionCard from './components/CollectionCard';
-import Header from './components/Header';
-import { useState, useEffect } from 'react';
-import axios from 'axios';
+import CollectionCard from './components/CollectionCard'
+import Header from './components/Header'
+import { useState, useEffect } from 'react'
+import axios from 'axios'
+import Punklist from './components/Punklist';
 
 
 function App() {
@@ -12,7 +13,7 @@ const [punkListData, setPunkListData] = useEffect([])
 
 useEffect(() => {
   const getMyNfts = async () => {
-    const openseaData = await axios.get('https://testnets-api.opensea.io/assets?assets_contract_address=0x93e19f97744bD1eeA86E70706F1BC510f19e6679&order_direction=acs'
+    const openseaData = await axios.get('https://testnets-api.opensea.io/assets?assets_contract_address=0x93e19f97744bD1eeA86E70706F1BC510f19e6679&order_direction=asc'
     )
     console.log(openseaData.data.assets)
     setPunkListData(openseaData.data.assets)
@@ -29,6 +30,7 @@ useEffect(() => {
       name={'Bandana Punk'} 
       traits={[{'value': 7}]} 
       image='https://ipfs.thirdweb.com/ipfs/QmZ5fD3UTRh8ALZCpMdypHkhMQSXyi4yyCz3Ea19kPmtXg/0.jpg' />
+
 
       <Punklist punkListData={punkListData} />
     </div>
--- src/components/PunkList.js
+++ src/components/PunkList.js
@@ -5,14 +5,16 @@ const PunkList = ({ punkListData }) => {
   return (
     <div className='punkList'>
         {punkListData.map(punk => (
-            <div>
-                <CollectionCard 
-                key={punk.token_id}
-                id={punk.token_id}
-                name={punk.name}
-                traits={punk.traits}
-                image={punk.image_original_url}
-                />   
+           <div>
+               <CollectionCard
+               key={punk.token_id}
+               id={punk.token_id}
+               name={punk.name}
+               traits={punk.traits}
+               image={punk.image_url}
+
+               />
+           </div> 
         ))}
     </div>
   )
